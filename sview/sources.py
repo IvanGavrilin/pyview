6e3124323363eb9cbebbcdaf00287da0c4e3ed11
Ivan Gavrilin <ivan.gavrilin@mail.ru>
2016-05-28 13:23:30 +0300

    Remove FileUpdater; Add RandomUpdater

--- sview/sources.py
+++ sview/sources.py
@@ -20,34 +20,33 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from os import stat
-import subprocess
 import datetime
 import traceback
-import sys
-from glob import glob
-from os import path
+import random
 
-from .plbfile import load_stream
-
-class FileUpdater:
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.pos = 0
-        self.st_mtime = None
-        self.st_mtime_ns = None
+class RandomUpdater:
+    def __init__(self):
+        self.limit = 0.2
+        self.initialized = False
 
     def is_update_needed(self, stream):
-        s = stat(self.filepath)
-        if s.st_mtime != self.st_mtime or s.st_mtime_ns != self.st_mtime_ns:
-            self.st_mtime = s.st_mtime
-            self.st_mtime_ns = s.st_mtime_ns
+        if not self.initialized:
+            return True
+        if random.random() < self.limit:
             return True
         return False
 
     def update(self, stream):
         try:
-            self.pos = load_stream(stream, self.filepath, self.pos)
+            if not self.initialized:
+                self.initialized = True
+                self.sig_a = stream.create_line_channel("sig_a", color="#6699CC", drawstyle='steps-pre', axis='top', axis_weight=0.8)
+                self.sig_b = stream.create_line_channel("sig_b", color="#DD3477", drawstyle='steps-pre', axis='top', axis_weight=0.8, fillstyle='bottom')
+
+            now = datetime.datetime.now().timestamp() * 1e6
+            self.sig_a.update_from_str(now, random.random())
+            self.sig_b.update_from_str(now, random.random())
+
         except Exception as e:
             print("Exception: ", e)
             print("======================================================================================")
